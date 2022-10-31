da824df4b70d9bacf54d827b668cd2a762fe9f49
Angie Sasmita <asasmita@google.com>
2022-10-31 20:42:52 +0800

    Compose version
    Change-Id: I4813ad41c4f94152411854d5964283e2fe98fb0b

---
+++ app/src/main/java/com/example/juicetracker/data/JuiceDao.kt
@@ -0,0 +1,41 @@
+/*
+ * Copyright (C) 2022 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.example.juicetracker.data
+
+import androidx.room.Dao
+import androidx.room.Delete
+import androidx.room.Insert
+import androidx.room.Query
+import androidx.room.Update
+import kotlinx.coroutines.flow.Flow
+
+/**
+ * Juice Data Access Object which contains methods to access and modify Juice table in Room DB
+ */
+@Dao
+interface JuiceDao {
+    @Query("SELECT * FROM juice")
+    fun getAll(): Flow<List<Juice>>
+
+    @Insert
+    suspend fun insert(juice: Juice)
+
+    @Delete
+    suspend fun delete(juice: Juice)
+
+    @Update
+    suspend fun update(juice: Juice)
+}
---
+++ app/src/main/java/com/example/juicetracker/ui/bottomsheet/EntryBottomSheet.kt
@@ -0,0 +1,197 @@
+/*
+ * Copyright (C) 2022 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.example.juicetracker.ui.bottomsheet
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material.Button
+import androidx.compose.material.Divider
+import androidx.compose.material.ExperimentalMaterialApi
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.ModalBottomSheetLayout
+import androidx.compose.material.ModalBottomSheetState
+import androidx.compose.material.OutlinedButton
+import androidx.compose.material.Text
+import androidx.compose.material.TextField
+import androidx.compose.material.TextFieldDefaults
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.input.ImeAction
+import androidx.compose.ui.unit.dp
+import com.example.juicetracker.R
+import com.example.juicetracker.data.Juice
+import com.example.juicetracker.data.JuiceColor
+import com.example.juicetracker.ui.JuiceTrackerViewModel
+import java.util.Locale
+
+@OptIn(ExperimentalMaterialApi::class)
+@Composable
+fun EntryBottomSheet(
+    juiceTrackerViewModel: JuiceTrackerViewModel,
+    sheetState: ModalBottomSheetState,
+    onCancel: () -> Unit,
+    onSubmit: () -> Unit,
+    modifier: Modifier = Modifier,
+    content: @Composable () -> Unit,
+) {
+    val juice by juiceTrackerViewModel.currentJuiceStream.collectAsState()
+
+    ModalBottomSheetLayout(
+        modifier = modifier,
+        sheetState = sheetState,
+        sheetContent = {
+            Column {
+                SheetHeader()
+                SheetForm(
+                    juice = juice,
+                    onUpdateJuice = juiceTrackerViewModel::updateCurrentJuice,
+                    onCancel = onCancel,
+                    onSubmit = onSubmit
+                )
+            }
+        }
+    ) {
+        content()
+    }
+}
+
+@Composable
+fun SheetHeader(modifier: Modifier = Modifier) {
+    Text(
+        modifier = modifier.padding(16.dp),
+        text = stringResource(R.string.bottom_sheet_headline),
+        style = MaterialTheme.typography.h5.copy(fontWeight = FontWeight.Bold)
+    )
+    Divider()
+}
+
+@Composable
+fun SheetForm(
+    juice: Juice,
+    onUpdateJuice: (Juice) -> Unit,
+    onCancel: () -> Unit,
+    onSubmit: () -> Unit,
+    modifier: Modifier = Modifier,
+) {
+    Column(modifier.padding(horizontal = 16.dp)) {
+        TextInputRow(
+            inputLabel = stringResource(R.string.juice_name),
+            fieldValue = juice.name,
+            onValueChange = { name -> onUpdateJuice(juice.copy(name = name)) }
+        )
+        TextInputRow(
+            inputLabel = stringResource(R.string.juice_description),
+            fieldValue = juice.description,
+            onValueChange = { description -> onUpdateJuice(juice.copy(description = description)) }
+        )
+        ColorSpinnerRow(
+            colorSpinnerPosition = JuiceColor.valueOf(juice.color).ordinal,
+            onColorChange = { color ->
+                onUpdateJuice(juice.copy(color = JuiceColor.values()[color].name))
+            }
+        )
+        RatingInputRow(
+            rating = juice.rating,
+            onRatingChange = { rating -> onUpdateJuice(juice.copy(rating = rating)) }
+        )
+        ButtonRow(
+            modifier = Modifier.align(Alignment.CenterHorizontally),
+            onCancel = onCancel,
+            onSubmit = onSubmit,
+            submitButtonEnabled = juice.name.isNotEmpty()
+        )
+    }
+}
+
+@Composable
+fun ButtonRow(
+    onCancel: () -> Unit,
+    onSubmit: () -> Unit,
+    submitButtonEnabled: Boolean,
+    modifier: Modifier = Modifier
+) {
+    Row(
+        modifier = modifier
+            .padding(bottom = 16.dp),
+        horizontalArrangement = Arrangement.spacedBy(24.dp)
+    ) {
+        OutlinedButton(
+            onClick = onCancel,
+            border = null
+        ) {
+            Text(stringResource(android.R.string.cancel).uppercase(Locale.getDefault()))
+        }
+        Button(
+            onClick = onSubmit,
+            enabled = submitButtonEnabled
+        ) {
+            Text(stringResource(R.string.save).uppercase(Locale.getDefault()))
+        }
+    }
+}
+
+@Composable
+fun TextInputRow(
+    inputLabel: String,
+    fieldValue: String,
+    onValueChange: (String) -> Unit,
+    modifier: Modifier = Modifier
+) {
+    InputRow(inputLabel, modifier) {
+        TextField(
+            value = fieldValue,
+            onValueChange = onValueChange,
+            singleLine = true,
+            maxLines = 1,
+            colors = TextFieldDefaults.textFieldColors(
+                backgroundColor = MaterialTheme.colors.surface
+            ),
+            keyboardOptions = KeyboardOptions(imeAction = ImeAction.Next)
+        )
+    }
+}
+
+@Composable
+fun InputRow(
+    inputLabel: String,
+    modifier: Modifier = Modifier,
+    content: @Composable () -> Unit
+) {
+    Row(
+        modifier = modifier.padding(bottom = 16.dp),
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+        Text(
+            text = inputLabel,
+            fontWeight = FontWeight.SemiBold,
+            modifier = modifier
+                .weight(1f)
+                .padding(end = 8.dp),
+        )
+        Box(modifier = Modifier.weight(3f)) {
+            content()
+        }
+    }
+}
