ced00a3a774f57ca39947b3c7ed819fd1026407d
schordas <schordas@google.com>
2023-06-05 12:43:15 -0700

    Material 3 compose interop migration
    Change-Id: I7000a7bc42066d0f9cf942a6259e6ba8f92a1d01

--- app/src/main/java/com/example/juicetracker/ui/JuiceListAdapter.kt
+++ app/src/main/java/com/example/juicetracker/ui/JuiceListAdapter.kt
@@ -1,11 +1,11 @@
 /*
- * Copyright (C) 2022 The Android Open Source Project
+ * Copyright (C) 2023 The Android Open Source Project
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- *      http://www.apache.org/licenses/LICENSE-2.0
+ *      https://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
@@ -25,10 +25,10 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
-import androidx.compose.material.Icon
-import androidx.compose.material.IconButton
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Text
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.ExperimentalComposeUiApi
@@ -104,7 +104,7 @@ fun JuiceDetails(juice: Juice, modifier: Modifier = Modifier) {
     Column(modifier, verticalArrangement = Arrangement.Top) {
         Text(
             text = juice.name,
-            style = MaterialTheme.typography.h5.copy(fontWeight = FontWeight.Bold),
+            style = MaterialTheme.typography.headlineSmall.copy(fontWeight = FontWeight.Bold),
         )
         Text(juice.description)
         RatingDisplay(rating = juice.rating, modifier = Modifier.padding(top = 8.dp))
