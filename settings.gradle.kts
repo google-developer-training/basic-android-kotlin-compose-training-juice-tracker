1eee8a1e889a9134da67512c1e267d992d8a0cfd
John Shea <sheajohn@google.com>
2023-07-20 23:46:52 +0300

    Migrate to Kotlin DSL

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -13,15 +13,19 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-// Top-level build file where you can add configuration options common to all sub-projects/modules.
-buildscript {
-    ext {
-        lifecycle_version = '2.6.1'
-        room_version = '2.5.1'
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
     }
 }
-plugins {
-    id 'com.android.application' version '8.0.2' apply false
-    id 'com.android.library' version '8.0.2' apply false
-    id 'org.jetbrains.kotlin.android' version '1.8.10' apply false
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
 }
+rootProject.name = "Juice Tracker"
+include(":app")
