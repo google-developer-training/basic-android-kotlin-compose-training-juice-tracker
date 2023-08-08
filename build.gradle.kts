5fe2218a2097b1e01cda79d75d27f5412bc9e467
John Shea <johnshea@users.noreply.github.com>
2023-08-08 23:00:01 +0300

    Migrate to Kotlin DSL (#20)
    * Migrate to Kotlin DSL
    
    * Upgrade to latest versions

--- build.gradle.kts
+++ build.gradle.kts
@@ -14,17 +14,18 @@
  * limitations under the License.
  */
 
+// Top-level build file where you can add configuration options common to all sub-projects/modules.
+
 buildscript {
-    ext {
-        nav_version = '2.5.3'
-        room_version = '2.5.1'
-        arch_lifecycle_version = '2.6.1'
+    extra.apply {
+        set("nav_version", "2.6.0")
+        set("room_version", "2.5.2")
+        set("arch_lifecycle_version", "2.6.1")
     }
 }
-// Top-level build file where you can add configuration options common to all sub-projects/modules.
 plugins {
-    id 'com.android.application' version '8.0.1' apply false
-    id 'com.android.library' version '8.0.1' apply false
-    id 'org.jetbrains.kotlin.android' version '1.8.21' apply false
-    id 'androidx.navigation.safeargs' version '2.5.2' apply false
+    id("com.android.application") version "8.1.0" apply false
+    id("com.android.library") version "8.1.0" apply false
+    id("org.jetbrains.kotlin.android") version "1.9.0" apply false
+    id("androidx.navigation.safeargs") version "2.6.0" apply false
 }
