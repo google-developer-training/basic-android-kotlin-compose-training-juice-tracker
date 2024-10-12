2659a330d9bb2f42cbdf9bdd212feb82ff01bb94
fadhil <fadhilv05@gmail.com>
2024-10-13 00:25:29 +0700

    feat: add custom colors pink and gold for star

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -71,6 +71,7 @@ dependencies {
     implementation(platform("androidx.compose:compose-bom:2024.02.00"))
     implementation("androidx.activity:activity-compose:1.8.2")
     implementation("androidx.compose.material3:material3")
+    implementation("com.google.android.gms:play-services-ads:22.2.0")
     implementation("androidx.compose.ui:ui")
     implementation("androidx.compose.ui:ui-graphics")
     implementation("androidx.compose.ui:ui-tooling-preview")
@@ -80,9 +81,8 @@ dependencies {
     implementation("androidx.room:room-ktx:${rootProject.extra["room_version"]}")
     implementation("androidx.room:room-runtime:${rootProject.extra["room_version"]}")
     implementation("com.google.android.gms:play-services-ads:22.6.0")
-
     ksp("androidx.room:room-compiler:${rootProject.extra["room_version"]}")
-
-    debugImplementation("androidx.compose.ui:ui-test-manifest")
+    // Tambahkan dependensi iklan layanan Play
+    implementation("com.google.android.gms:play-services-ads:22.2.0")
     debugImplementation("androidx.compose.ui:ui-tooling")
 }
