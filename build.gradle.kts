f676050b6edf5df6c47d7a77fb02e748685c9d50
Jolanda Verhoef <JolandaVerhoef@users.noreply.github.com>
2024-12-09 15:27:11 +0000

    Update Kotlin and KSP

--- build.gradle.kts
+++ build.gradle.kts
@@ -25,6 +25,6 @@ buildscript {
 plugins {
     id("com.android.application") version "8.7.3" apply false
     id("com.android.library") version "8.7.3" apply false
-    id("org.jetbrains.kotlin.android") version "1.9.22" apply false
+    id("org.jetbrains.kotlin.android") version "2.1.0" apply false
     id("androidx.navigation.safeargs") version "2.8.4" apply false
 }
