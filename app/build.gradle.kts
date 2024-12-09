c8eeb50f5b201c507646ee72431eda21c1a4aafa
Jolanda Verhoef <JolandaVerhoef@users.noreply.github.com>
2024-12-09 15:03:14 +0000

    update compose compiler

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -18,6 +18,7 @@ plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
     id("com.google.devtools.ksp") version "2.1.0-1.0.29"
+    id("org.jetbrains.kotlin.plugin.compose")
 }
 
 android {
@@ -55,9 +56,6 @@ android {
     }
     buildFeatures {
         compose = true
-    }
-    composeOptions {
-        kotlinCompilerExtensionVersion = "1.5.15"
     }
     packaging {
         resources {
